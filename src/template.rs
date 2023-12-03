ac3b5fc4586a0a2a4db1849d77892a60d601b735
Maxwell Huenink <maxh76@users.noreply.github.com>
2023-12-03 15:15:04 -0600

    Create a template for future days

---
+++ src/template.rs
@@ -0,0 +1,23 @@
+use crate::lines::read;
+
+pub fn run_part1() {
+    // Read lines of input
+    if let Ok(lines) = read("./inputs/dayX/test1.txt") {
+        // Iterate over the lines
+        for line_result in lines {
+            // If the line is okay
+            if let Ok(line) = line_result {}
+        }
+    }
+}
+
+pub fn run_part2() {
+    // Read lines of input
+    if let Ok(lines) = read("./inputs/dayX/test2.txt") {
+        // Iterate over the lines
+        for line_result in lines {
+            // If the line is okay
+            if let Ok(line) = line_result {}
+        }
+    }
+}
