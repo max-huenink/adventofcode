67dce3a92ebfebad3f46d981293a0681dc4cc8f8
Max Huenink <maxh76@users.noreply.github.com>
2025-12-05 20:04:12 -0600

    feat: 2025 day 1!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod year2023;
+mod year2025;
 
 fn main() {
-    let supported_puzzles = ["2023/1", "2023/2", "2023/3", "2024/1"];
+    let supported_puzzles = ["2023/1", "2023/2", "2023/3", "2025/1"];
     println!("Supported puzzles are: ");
     for supported in supported_puzzles {
         println!("{supported}");
@@ -30,6 +31,8 @@ fn main() {
         [2023, 2, 2] => year2023::day2::run_part2,
         [2023, 3, 1] => year2023::day3::run_part1,
         [2023, 3, 2] => year2023::day3::run_part2,
+        [2025, 1, 1] => year2025::day1::run_part1,
+        [2025, 1, 2] => year2025::day1::run_part2,
         _ => panic!("Puzzle not supported, you entered {puzzle_parts:?}"),
     };
 
