4757d2e1bb9784a2e2def8c4920c9a0beffe71f9
Max Huenink <maxh76@users.noreply.github.com>
2025-12-08 20:57:57 -0600

    fix: Simplify day 3 part 2

--- src/year2025/day3.rs
+++ src/year2025/day3.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 pub fn run_part1(input: Vec<String>) {
     let answer = input
         .iter()
@@ -17,8 +15,10 @@ pub fn run_part1(input: Vec<String>) {
                 }
             }
             println!("{line} {first:?} {second:?}");
-            if first.1.is_digit(10) && second.1.is_digit(10) {
-                return first.1.to_digit(10).unwrap() * 10 + second.1.to_digit(10).unwrap();
+            if let Some(first_digit) = first.1.to_digit(10)
+                && let Some(second_digit) = second.1.to_digit(10)
+            {
+                return first_digit * 10 + second_digit;
             }
 
             0
@@ -32,29 +32,29 @@ pub fn run_part2(input: Vec<String>) {
     let answer = input
         .iter()
         .map(|line| {
-            let mut str = String::new();
-            let mut all = [(0, 0 as u128); 12];
+            let mut joltage = 0;
+            let mut indices = [0; 12];
             for idx in 0..12 {
-                let skip_amt = if idx == 0 { 0 } else { all[idx - 1].0 + 1 };
+                let mut current = 0;
+                let skip_amt = if idx == 0 { 0 } else { indices[idx - 1] + 1 };
                 let take_amt = line.len() - (skip_amt + (12 - idx));
                 for c in line.char_indices().skip(skip_amt).take(take_amt) {
-                    if let Some(num) = c.1.to_digit(10) {
-                        let num2 = num as u128;
-                        if num2.cmp(&all[idx].1).is_gt() {
-                            all[idx] = (c.0, num2);
+                    if let Some(num) = c.1.to_digit(10).map(|d| d as u64) {
+                        if num > current {
+                            current = num;
+                            indices[idx] = c.0;
+                        }
+                        if current == 9 {
+                            continue;
                         }
                     }
                 }
-                str.push_str(&all[idx].1.to_string());
+                joltage += 10_u64.pow(11 - idx as u32) * current;
             }
 
-            if let Ok(val) = str.parse::<u128>() {
-                return val;
-            }
-
-            0
+            joltage
         })
-        .sum::<u128>();
+        .sum::<u64>();
 
     println!("{answer}");
 }
