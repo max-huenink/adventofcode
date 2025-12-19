924d59d95f0f696183326e94b5bf369f7993c711
Maxwell Huenink <6878333+max-huenink@users.noreply.github.com>
2025-12-18 22:29:14 -0600

    fix: Day 6.2 progres..

--- src/year2025/day6.rs
+++ src/year2025/day6.rs
@@ -1,7 +1,7 @@
 pub fn run_part1(input: &[String]) {
     let mut numbers = Vec::<Vec<i64>>::new();
     let mut operations = Vec::<char>::new();
-    // for line in input {
+
     for line in input {
         let mut columns = line
             .split(char::is_whitespace)
@@ -13,12 +13,14 @@ pub fn run_part1(input: &[String]) {
                 if idx >= numbers.len() {
                     numbers.push(vec![]);
                 }
+
                 numbers[idx].push(num);
             } else if cell.len() == 1
                 && let Some(op) = cell.chars().next()
             {
                 operations.push(op);
             }
+
             idx += 1;
         }
     }
@@ -39,4 +41,65 @@ pub fn run_part1(input: &[String]) {
     println!("{sum}");
 }
 
-pub fn run_part2(input: &[String]) {}
+pub fn run_part2(input: &[String]) {
+    let mut numbers = Vec::<Vec<&str>>::new();
+    let mut operations = Vec::<char>::new();
+
+    for line in input {
+        let mut columns = line
+            .split(char::is_whitespace)
+            .filter(|s| !s.trim().is_empty());
+
+        let mut idx = 0;
+        while let Some(cell) = columns.next() {
+            if let Ok(_) = cell.parse::<i64>() {
+                if idx >= numbers.len() {
+                    numbers.push(vec![]);
+                }
+
+                numbers[idx].push(cell);
+            } else if cell.len() == 1
+                && let Some(op) = cell.chars().next()
+            {
+                operations.push(op);
+            }
+
+            idx += 1;
+        }
+    }
+
+    let mut sum = 0;
+    let mut idx = 0;
+    let number_columns = numbers.iter().map(|col| col.iter().map(|c| c.chars()));
+
+    for mut col in number_columns {
+        let op = operations[idx];
+        let mut acc = 0;
+        let mut f = true;
+
+        while f {
+            f = false;
+            let mut new_number = 0;
+
+            for mut cell in &mut col {
+                if let Some(c) = cell.next()
+                    && let Some(d) = c.to_digit(10)
+                {
+                    new_number += (new_number * 10) + d;
+                    f = true;
+                }
+            }
+
+            match op {
+                '*' => acc *= new_number,
+                '+' => acc += new_number,
+                _ => (),
+            };
+        }
+
+        sum += acc;
+        idx += 1;
+    }
+
+    println!("{sum}");
+}
